293e128d078497049ef9ecc5bee8df1a2192e9e5
numb86 <86numb@gmail.com>
2017-01-09 16:03:21 +0900

    search-placeを実装。

--- iroha.lisp
+++ iroha.lisp
@@ -1,3 +1,6 @@
+(defparameter *hiragana* "あいうえお")
+(defparameter *katakana* "アイウエオ")
+
 (defun myFunc (arg)
   (setq arg (convert-string arg nil nil))
   (print arg))
@@ -21,5 +24,24 @@
         (convert-string arg nil result)))))
 
 
+(defun comparison (a b order-list)
+  (let ((a-place nil)(b-place nil))
+    (print a)))
+
+(defun search-place (word num order-list)
+  (dotimes (i (length order-list) nil)
+    (let ((target nil)(place nil))
+      (setq target (subseq word num (+ 1 num)))
+      (setq place (search target (nth i order-list)))
+      (if place
+          (return-from search-place place))))
+  (return-from search-place nil))
+
 ; (myFunc '(文文文　字　字列a bb))
-(myFunc '(文文文　字　字 列 a bb))
+; (myFunc '(文文文　字　字 列 a bb))
+
+; (comparison "アルファベット" "いろは" nil)
+
+; (print (search-place "いろは" 0 (list *hiragana* *katakana*)))
+(print (search-place "アルファベット" 0 (list *hiragana* *katakana*)))
+
